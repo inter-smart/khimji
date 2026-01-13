26fffdefbb6e648cae7c8fbb8f754183653d2783
afsalmern <afsal@intersmart.in>
2026-01-13 15:00:05 +0530

    removed cache

--- src/lib/server/api.js
+++ src/lib/server/api.js
@@ -20,8 +20,6 @@ export async function getData(endpoint, lang = "en", country = null, options = {
   }
 
   const defaultOptions = {
-    cache: "force-cache",
-    next: { revalidate: 600 },
     headers: {
       "Content-Type": "application/json",
       "Location-Slug": country || DEFAULT_COUNTRY,
