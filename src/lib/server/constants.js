371c170a972b6544f7851cdc8e937fc8a398cb1b
Nikhil-Govind-Inter <nikhil@intersmart.in>
2026-01-20 12:22:57 +0530

    setup default country as oman

--- src/lib/server/constants.js
+++ src/lib/server/constants.js
@@ -140,5 +140,5 @@ export const defaultMeta = {
 export const DefaultOgImage = "/images/logo.png";
 
 export const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/` || "http://localhost:3001";
-export const DEFAULT_COUNTRY = "united-arab-emirates";
+export const DEFAULT_COUNTRY = "oman";
 export const DEFAULT_BUSINESS_TYPE = "b2b";
